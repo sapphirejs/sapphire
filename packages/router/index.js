e7926de53dae5d44c43e2182ab8550812d2b6e08
Aleksander Koko <aleksanderkoko@gmail.com>
2018-03-21 14:56:17 +0100

    remove typo from router index

--- packages/router/index.js
+++ packages/router/index.js
@@ -2,8 +2,6 @@ const Route = require('./lib/route')
 const Settings = require('./lib/settings')
 const InvalidRouteArguments = require('./lib/errors/invalid-route-arguments')
 
-const
-
 module.exports = {
   Route,
   InvalidRouteArguments,
