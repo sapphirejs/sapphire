9e78e1a3c5e62bc0f2ef16932e9f9db40d34fc8d
Aleksander Koko <aleksanderkoko@gmail.com>
2018-03-21 14:53:23 +0100

    add settings on router export

--- packages/router/index.js
+++ packages/router/index.js
@@ -1,7 +1,11 @@
 const Route = require('./lib/route')
+const Settings = require('./lib/settings')
 const InvalidRouteArguments = require('./lib/errors/invalid-route-arguments')
+
+const
 
 module.exports = {
   Route,
-  InvalidRouteArguments
+  InvalidRouteArguments,
+  Settings
 }
