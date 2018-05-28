588985fd1664cbc8a2c46bbe33bbf360b45bbb63
Fadion Dashi <jonidashi@gmail.com>
2018-05-28 14:04:37 +0200

    Validator: Expose ValidationError
    Signed-off-by: Fadion Dashi <jonidashi@gmail.com>

--- packages/validator/index.js
+++ packages/validator/index.js
@@ -1,9 +1,11 @@
 const Validator = require('./lib/validator')
 const Middleware = require('./lib/middleware')
+const ValidationError = require('./lib/errors/validation-error')
 const Joi = require('joi')
 
 module.exports = {
   rule: Joi,
   Validator,
-  ValidatorMiddleware: Middleware
+  ValidatorMiddleware: Middleware,
+  ValidationError
 }
